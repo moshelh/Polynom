10394fb05173f76862a421583508349ae372b44b
User <User@DESKTOP-5A9JP4K>
2018-11-13 15:23:30 +0200

    adding explanations

--- src/myMath/LinePlotTest.java
+++ src/myMath/LinePlotTest.java
@@ -13,6 +13,11 @@ import de.erichseifert.gral.plots.lines.LineRenderer;
 import de.erichseifert.gral.plots.points.PointRenderer;
 import de.erichseifert.gral.ui.InteractivePanel;
 import de.erichseifert.gral.plots.colors.*;
+/**
+ * plotting  polynom graph and in addition prints the area under x axis.
+ * source:http://trac.erichseifert.de/gral/wiki/Tutorials/XYPlot
+ *
+ */
 public class LinePlotTest extends JFrame {
     public LinePlotTest() {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -28,7 +33,7 @@ public class LinePlotTest extends JFrame {
             double y=p.f(x);
             //finding the vertex
             if ((p.derivative().f(x)>0)&&(p.derivative().f(x)<0.5)) {
-            	data1.add(x,y);
+            	data1.add(x,y); //adding the vertex to data 1 and the other points to data.
             	
             	System.out.println("x="+x+","+"y="+y);
             }else {
@@ -37,7 +42,7 @@ public class LinePlotTest extends JFrame {
         }
         //print the area under the x axis ,using the function from the polynom.
        System.out.println("the area under x axis is:"+p.areaUnderXaxis(-0.94064,4.83104 , 0.01));
-        XYPlot plot = new XYPlot(data,data1);
+        XYPlot plot = new XYPlot(data,data1);  //plot with data and data1.
         getContentPane().add(new InteractivePanel(plot));
         LineRenderer lines = new DefaultLineRenderer2D();
         plot.setLineRenderers(data, lines);
@@ -45,6 +50,7 @@ public class LinePlotTest extends JFrame {
        Color color = new Color(0.0f, 0.3f, 1.0f);
        Color color1 = new Color(0.0f, 0.3f, 1.0f);
        plot.getPointRenderers(data).get(0).setColor(color.RED);
+       //setting color for data1,the vertex ,to black.
        plot.getPointRenderers(data1).get(0).setColor(color.BLACK);
        plot.getLineRenderers(data).get(0).setColor(color);  
     }
