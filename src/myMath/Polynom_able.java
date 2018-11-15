6737a269b21b56fc3aff7289a03f66a5da2cf6d5
Moshe El-hadad <moshelh95@gamil.com>
2018-11-15 18:59:31 +0200

    add java doc

--- src/myMath/Polynom_able.java
+++ src/myMath/Polynom_able.java
@@ -4,7 +4,7 @@ import java.util.Iterator;
 
 /**
  * This interface represents a general Polynom: f(x) = a_1X^b_1 + a_2*X^b_2 ... a_n*Xb_n,
- * where: a_1, a_2 ... a_n are real numbers and b_1<b_2..<b_n >=0 are none negative integers (naturals)
+ * where: a_1, a_2 ... a_n are real numbers and b_1{b_2..{b_n }=0 are none negative integers (naturals)
  * For formal definitions see: https://en.wikipedia.org/wiki/Polynomial
  * 
  * Such polygon has the following functionality:
@@ -23,8 +23,8 @@ import java.util.Iterator;
  * 3.2 Polynom derivative() // returns a new Polygon of the derivative ("NIGZERET").
  * 3.3 double f(x) // return this Polygon value at p(x) 
  * 3.4 boolean equals(Polygon p) // returns true iff for any x: this.f(x) == p.f(x)
- * 3.5 double root(double x0, double x1, double eps) // assuming (f(x0)*f(x1)<=0, returns f(x2) such that:
- *													//	(i) x0<=x2<=x2 & (ii) {f(x2)<eps
+ * 3.5 double root(double x0, double x1, double eps) // assuming (f(x0)*f(x1){=0, returns f(x2) such that:
+ *													//	(i) x0{=x2{=x2 and (ii) {f(x2){eps
  * 3.6 String toString() // returns a String such that it can be used for init an equal(s) Polygon
  *													
  * 
@@ -35,7 +35,7 @@ import java.util.Iterator;
 public interface Polynom_able extends cont_function{
 	/**
 	 * Add p1 to this Polynom
-	 * @param p1 
+	 * @param p1 Polynom_able
 	 */
 	public void add(Polynom_able p1);
 	/**
@@ -45,12 +45,12 @@ public interface Polynom_able extends cont_function{
 	public void add(Monom m1);
 	/**
 	 * Subtract p1 from this Polynom
-	 * @param p1
+	 * @param p1 Polynom_able
 	 */
 	public void substract(Polynom_able p1);
 	/**
 	 * Multiply this Polynom by p1
-	 * @param p1
+	 * @param p1 Polynom_able
 	 */
 	public void multiply(Polynom_able p1);
 	/**
@@ -65,9 +65,9 @@ public interface Polynom_able extends cont_function{
 	 */
 	public boolean isZero();
 	/**
-	 * Compute a value x' (x0<=x'<=x1) for with |f(x')| < eps
-	 * assuming (f(x0)*f(x1)<=0, returns f(x2) such that:
-	 * *	(i) x0<=x2<=x2 && (ii) f(x2)<eps
+	 * Compute a value x' (x0{=x'{=x1) for with |f(x')| { eps
+	 * assuming (f(x0)*f(x1){=0, returns f(x2) such that:
+	 * *	(i) x0{=x2{=x2 and (ii) f(x2){eps
 	 * @param x0 starting point
 	 * @param x1 end point
 	 * @param eps step (positive) value
